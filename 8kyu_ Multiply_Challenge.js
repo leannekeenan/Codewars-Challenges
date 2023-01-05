4b1291017965353f357d59f53d2f3fa0605e414f
Leanne Keenan <leanne.keenan1@gmail.com>
2023-01-05 12:56:40 -0800

    Added Daily Codewars Challenge - Multiply

---
+++ 8kyu_ Multiply_Challenge.js
@@ -0,0 +1,7 @@
+/*
+This code does not execute properly. Try to figure out why. */
+
+function multiply(a, b){
+    return a * b
+  }
+  /*the reason the internal action would not run  was that it was not provided a return or console.log() to print the results*/
