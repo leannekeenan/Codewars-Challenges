79b2a36b046c11ba6979d85c05075f50b491b391
Leanne Keenan <leanne.keenan1@gmail.com>
2023-01-11 18:27:44 -0800

    Added Daily Codewars Challenge
    8kyu - Reversed Strings
    Uses .split(), .reverse(), and .join() to take a word apart reverse it, and put it back together

---
+++ 8kyu-Reversed_Strings.js
@@ -0,0 +1,12 @@
+/*
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+STRINGS
+*/
+
+function solution(str){
+    return str = str.split('').reverse().join('');
+      
+    }
