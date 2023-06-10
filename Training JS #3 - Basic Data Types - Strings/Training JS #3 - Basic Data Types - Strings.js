97abd2543563435b2cba80ced26a78bcdd04c464
Leanne Keenan <l_keenan@outlook.com>
2023-06-10 13:49:38 -0700

    Add files via upload
    mission 1: I've created three functions, and defined some global variables, please select some variables that can make up the name of the function, and return them(Please note the uppercase and lowercase letters are different).
    
    mission 2: After mission 1 finished. you should click "Attempt" to see my three questions, and write the answer in function answer1, answer2,answer3

---
+++ Training JS #3 - Basic Data Types - Strings/Training JS #3 - Basic Data Types - Strings.js
@@ -0,0 +1,56 @@
+/*
+misson 1: I've create three function, and defined some global variables, please select some variables that can make up the name of the function, and return them(Please note the uppercase and lowercase letters are different).
+
+When you have finished the work, click "Run Tests" to see if your code is working properly.
+
+misson 2: After misson 1 finished. you should click "Attempt" to see my three questions, and write the answer in function answer1, answer2,answer3
+*/
+
+var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
+function Dad(){
+  //select some variable to combine "Dad"
+  return d1 + a2 + d2;
+}
+function Bee(){
+  //select some variable to combine "Bee"
+  return b1 + e2 + e2;
+}
+function banana(){
+  //select some variable to combine "banana"
+  return b2 + a2 + n2 + a2 + n2 + a2;
+}
+
+//answer some questions if you finished works above
+/*
+John's iron axe fell into the river. He cried sadly by the river.
+Then a monster came out of the water, with a golden axe in his hand.
+He asked John: is this golden axe yours?
+*/
+function answer1(){
+  //the answer should be "yes" or "no"
+  return "no";
+}
+
+/*
+John replied that it was not his axe. The monster took out a silver axe.
+He asked John: is this silver axe yours?
+*/
+
+function answer2(){
+  //the answer should be "yes" or "no"
+  return "no"
+}
+
+/*
+John replied that it was not his axe. The monster took out a iron axe.
+He asked John: is this iron axe yours?
+*/
+function answer3(){
+  //the answer should be "yes" or "no"
+  return "yes";
+}
+/*
+John said happily: This is my iron axe!!!
+
+The monster smiled and said to John, "you are an honest boy!"
+*/
