e1ff22e60bcca849514e9bdac53fecd83b2a075e
Leanne Keenan <l_keenan@outlook.com>
2023-07-13 17:37:30 -0700

    Added Daily Codewars Challenge
    Write a 'welcome' function that takes a parameter 'language' (always a string), and returns a greeting - if you have it in your database. It should default to English if the language is not in the database, or in the event of an invalid input.

---
+++ Welcome/Welcome.js
@@ -0,0 +1,32 @@
+/*
+Write a 'welcome' function that takes a parameter 'language' (always a string), and returns a greeting - if you have it in your database. It should default to English if the language is not in the database, or in the event of an invalid input.
+*/
+
+function greet(language) {
+	let welcome = {
+    english: 'Welcome',
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso'
+  }
+  
+  if(welcome.hasOwnProperty(language)) {
+    return welcome[language]
+  }
+  else {
+    return 'Welcome'
+  }
+}
