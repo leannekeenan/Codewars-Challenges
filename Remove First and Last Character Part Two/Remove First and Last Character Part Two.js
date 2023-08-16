c4e387b5c4de4170c8d5c9861bc9d0708ce8eb96
Leanne Keenan <l_keenan@outlook.com>
2023-08-16 16:37:20 -0700

    Added Daily Codewars Challenge
    You are given a string containing a sequence of character sequences separated by commas.
    
    Write a function that returns a new string containing the same character sequences except the first and the last ones but this time separated by spaces.
    
    If the input string is empty or the removal of the first and last items would cause the resulting string to be empty, return an empty value

---
+++ Remove First and Last Character Part Two/Remove First and Last Character Part Two.js
@@ -0,0 +1,20 @@
+/*
+You are given a string containing a sequence of character sequences separated by commas.
+
+Write a function which returns a new string containing the same character sequences except the first and the last ones but this time separated by spaces.
+
+If the input string is empty or the removal of the first and last items would cause the resulting string to be empty, return an empty value
+*/
+
+
+function array(string) {
+    if(string === '' || string.split(',').length < 3) {
+      return null;
+    }
+    else {
+      let sequence = string.split(',');
+        sequence.shift();
+        sequence.pop();
+        return sequence.join(' ');
+    }
+  }
