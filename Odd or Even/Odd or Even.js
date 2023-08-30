7eea41abfff4a5aa6c8bd62aaf2401fa124786ac
Leanne Keenan <l_keenan@outlook.com>
2023-08-30 14:12:43 -0700

    Added Daily Codewars Challenge
    Given a list of integers, determine whether the sum of its elements is odd or even.
    
    Give your answer as a string matching "odd" or "even".
    
    If the input array is empty consider it as: [0] (array with a zero).

---
+++ Odd or Even/Odd or Even.js
@@ -0,0 +1,17 @@
+/*
+Given a list of integers, determine whether the sum of its elements is odd or even.
+
+Give your answer as a string matching "odd" or "even".
+
+If the input array is empty consider it as: [0] (array with a zero).
+*/
+
+function oddOrEven(array) {
+    let total = array.reduce((acc, curr) => acc + curr, 0);
+    if(total % 2 === 0) {
+     return 'even';
+    }
+    else {
+     return 'odd'
+    }
+ }
